7d43c6bb5d687076a2a36dd70984ba11df4f41b6
DenWed4a <73389565+DenWed4a@users.noreply.github.com>
2022-02-01 02:54:25 +0300

    Delete hostel-web-app/src/main/webapp directory

--- hostel-web-app/src/main/webapp/js/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-function getUrl(){
-	var actionURL = '<jsp:include page="userList.jsp"></jsp:include>';
-	var actionURL2 = 'asdasd'
-	var infoField = document.getElementById('ooo');
-	infoField.innerHTML = actionURL;	
-}
